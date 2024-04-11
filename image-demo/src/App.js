8db326b3cabee561acc5c41b20f3c9fbd98a7d95
ravishnu60 <ravishnu60@gmail.com>
2024-04-11 15:30:38 +0530

    new

--- image-demo/src/App.js
+++ image-demo/src/App.js
@@ -9,13 +9,13 @@ function App() {
       </div>
       <div className='container-fluid d-flex justify-content-center'>
         <div style={{ marginTop: '5px',overflow:'hidden', padding:'10px' }}>
-          <p className='small'>Church location</p>
+          <p className='mb-0 small'>Church location</p>
           <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Our%20Lady%20of%20Refugee%20-%20Elathagiri,%20church,%20Elathagiri,%20India+(My%20Business%20Name)&amp;t=k&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
             <a href="https://www.gps.ie/">gps devices</a>
           </iframe>
         </div>
         <div style={{ marginTop: '5px',overflow:'hidden', padding:'10px'  }}>
-          <p className='small'>Reception location</p>
+          <p className='mb-0 small'>Reception location</p>
           <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Sonia%20Kevin%20mahal%20Marriage%20Hall%20Sundampati%20chennai%20salai%20Krishnagiri%20dt%20ah%20wedding+(My%20Business%20Name)&amp;t=k&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
             <a href="https://www.gps.ie/">gps tracker sport</a>
           </iframe>
